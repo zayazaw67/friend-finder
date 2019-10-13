1162a71c9f21732cbf0dc7ba35159fc6eea998ee
Zaya <zayazaw67@gmail.com>
2019-10-13 16:34:54 -0500

    added another object, template for home/survey added

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,5 +3,10 @@ let friends = [
         "name": "Ahmed",
         "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    },
+    {
+        "name": "John",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [1, 5, 2, 2, 1, 4, 4, 2, 1, 3]
     }
 ]
