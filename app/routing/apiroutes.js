5e684f42a3b8ab9c580c705428bc8c614048615d
Zaya <zayazaw67@gmail.com>
2019-10-14 17:07:23 -0500

    created match logic, linked to server

--- app/routing/apiroutes.js
+++ app/routing/apiroutes.js
@@ -2,3 +2,50 @@ var path = require('path');
 var friendsArray = require('../data/friends.js');
 
 // console.log(friendsArray)
+
+function friendMatcher(app) {
+    // displays all friends
+    app.get('/api/friends', function (req, res) {
+        res.json(friendsArray);
+    });
+
+    // for adding new friend
+    app.post('/api/friends', function (req, res) {
+        // grab user input
+        var newEntry = {
+            name: req.body.name,
+            photo: req.body.photo,
+            total: []
+        };
+        var answers = [];
+        for (var i = 0; i < req.body.total.length; i++) {
+            answers.push(parseInt(req.body.total[i]))
+        }
+        newEntry.total = answers;
+        // checking new entry with friends.js data
+        var scoreComparison = [];
+        for (var i = 0; i < friendsArray.length; i++) {
+            // math for checking score
+            var score = 0;
+            for (var j = 0; j < newEntry.total.length; j++) {
+                score += Math.abs(newEntry.total[j] - friendsArray[i].total[j]);
+            }
+            scoreComparison.push(score);
+        }
+        // Determine the best match using the postion of best match in the friendsArray array
+        var bestMatch = 0; // start with first object in array
+        for (var i = 1; i < scoreComparison.length; i++) {
+            // checks for lowest score for best match
+            if (scoreComparison[i] <= scoreComparison[bestMatch]) {
+                bestMatch = i;
+            }
+        }
+        var bestFriendMatch = friendsArray[bestMatch];
+        // json
+        res.json(bestFriendMatch);
+        // takes user input and adds as new data 
+        friendsArray.push(newEntry);
+    });
+}
+
+module.exports = friendMatcher;
--- server.js
+++ server.js
@@ -1,17 +1,19 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 
 let PORT = process.env.PORT || 8080;
 
 let app = express();
-app.use(express.static(path.join(__dirname, "app/public")));
+app.use(express.static(path.join(__dirname, 'app/public')));
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./app/routing/htmlroutes.js")(app);
+require('./app/routing/htmlroutes.js')(app);
+require('./app/routing/apiroutes.js')(app);
+
 
 app.listen(PORT, function () {
-  console.log("Server listening on: http://localhost:" + PORT);
+  console.log('Server listening on: http://localhost:' + PORT);
 });
